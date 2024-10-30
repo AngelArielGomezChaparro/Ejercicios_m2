e3348a4596e436556dd4297d4ebe12dbeaf5ed6c
AngelArielGomezChaparro <182712613+AngelArielGomezChaparro@users.noreply.github.com>
2024-10-30 12:25:16 -0500

    ejercicio 2

--- ejercicios_m2_1.py
+++ ejercicios_m2_1.py
@@ -1,3 +1,4 @@
-name = input("Enter your name")
+radius = float(input("What's the radius of the circle?: "))
 
-print(f"Hello {name}")
+print(f"The circumference of the circle is {radius * 2 * 3.14}.")
+print(f"The area of the circle is {3.14 * radius * radius}.")
